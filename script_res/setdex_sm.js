5d441c2eaa56aa901a3f46175733a2f0380f145d
AWildNoob <a.fahcci@gmail.com>
2017-04-04 19:40:57 -0400

    Update setdex_sm.js

--- script_res/setdex_sm.js
+++ script_res/setdex_sm.js
@@ -1,3 +1,6 @@
 var SETDEX_SM={
- 
+ "Abra": {
+		"LC Focus Sash": {"level":5,"evs":{"at":36,"sp":200,"sa":240},"nature":"Timid","ability":"Magic Guard","item":"Focus Sash","moves":["Psychic","Dazzling Gleam","Hidden Power Fighting","Protect"]},
+		"LC Life Orb": {"level":5,"evs":{"sp":200,"sa":240},"nature":"Timid","ability":"Magic Guard","item":"Life Orb","moves":["Psychic","Dazzling Gleam","Hidden Power Fighting","Substitute"]},
+	},
 };
