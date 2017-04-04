20cd3a3c4696f463402422254372beb738ff04e1
AWildNoob <a.fahcci@gmail.com>
2017-04-04 19:48:08 -0400

    Update setdex_sm.js

--- script_res/setdex_sm.js
+++ script_res/setdex_sm.js
@@ -1,23 +1,23 @@
 var SETDEX_SM={
  "Abra": {
-		"LC Focus Sash": {
-			"level":5,
-			"evs":{
-				"hp":0,
-				"at":36,
-				"def":0,
-				"sa":240,
-				"sd":0,
-				"sp":200
-				},
-			"nature":"Timid",
-			"ability":"Magic Guard",
-			"item":"Focus Sash",
-			"moves":["Psychic",
-				 "Dazzling Gleam",
-				 "Hidden Power Fighting",
-				 "Protect"
-				]
+	"LC Focus Sash": {
+		"level":5,
+		"evs":{
+			"hp":0,
+			"at":36,
+			"def":0,
+			"sa":240,
+			"sd":0,
+			"sp":200
 			},
+		"nature":"Timid",
+		"ability":"Magic Guard",
+		"item":"Focus Sash",
+		"moves":["Psychic",
+			 "Dazzling Gleam",
+			 "Hidden Power Fighting",
+			 "Protect"
+			]
+		},
 	},
 };
